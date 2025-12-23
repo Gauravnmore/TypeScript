4d61c9b7aff70fde575f8ac1e82eae4958eeaf76
Gaurav Nitin More <106823295+Gauravnmore@users.noreply.github.com>
2025-12-23 13:20:42 +0530

    Update datatype.ts

--- datatype.ts
+++ datatype.ts
@@ -3,3 +3,12 @@ let stringEx : string = "gaurav more";
 
 console.log("given number is:",numberEx);
 console.log("given string is:",stringEx);
+
+/*let numberEx : number;
+let stringEx : string;
+
+numberEx  =42;
+stringEx  = "gaurav more";
+
+console.log("given number is:",numberEx);
+console.log("given string is:",stringEx);*/
