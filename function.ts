d673061614565e98e4b4b7a3dadbe75773a29407
Gaurav Nitin More <106823295+Gauravnmore@users.noreply.github.com>
2025-12-23 17:26:55 +0530

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,8 @@
+function areaofrectangle(lengthrectangle :number,widthrectangle:number){
+    return lengthrectangle*widthrectangle;
+}
+const lengthrectangle =12;
+const widthrectangle =6;
+const area=areaofrectangle(lengthrectangle,widthrectangle);
+
+console.log(`area of the rectangle is: ${area}`);
