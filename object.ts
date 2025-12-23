47f6a548d43085f772546f7970be520e18daab68
Gaurav Nitin More <106823295+Gauravnmore@users.noreply.github.com>
2025-12-23 13:39:02 +0530

    Add files via upload

---
+++ object.ts
@@ -0,0 +1,16 @@
+let personEx: {
+    name: string;
+    age: number;
+    jobTitle?: string;
+    address: {
+        street: string;
+        city: string;};
+} = {
+    name: "gaurav more",
+    age: 22,
+    jobTitle: "MCA",
+    address: {
+        street: "SMIT collage",
+        city: "Jalgaon",},
+};
+console.log("given person detail is:",personEx);
